8f14b7968405e9b1254a098947546623c61b34d6
shoaibnycda <shoaib@adrepublicbd.com>
2017-07-18 14:03:45 -0400

    second commit

---
+++ Quizzler/Question.swift
@@ -0,0 +1,22 @@
+// copywrite Shoaib Ahamed
+// wanna email? : shoaib@adrepublicbd.com
+
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text : String, correctAnswer : Bool) {
+    
+    questionText = text
+    answer = correctAnswer
+        
+        
+    }
+    
+    
+}
+
